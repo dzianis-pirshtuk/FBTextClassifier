515d9a4449b0e30fca3c16aa3a449969521d0c83
Daniel MacLennan <dsmaclen@umich.edu>
2014-03-22 16:11:28 -0400

    Made a truncate script that also decodes into utf-8

---
+++ truncate.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import os.path
+
+def main():
+    usage = sys.argv[0] + ' <Input CSV File> <Output CSV File> <Number of lines to keep>'
+    
+    if len(sys.argv) != 4:
+        print usage
+        print 'Invalid number of arguments'
+        sys.exit(1)
+    
+    try:
+        inFile = open(sys.argv[1], 'r')
+    except:
+        print usage
+        print 'Error opening input CSV file'
+        sys.exit(1)
+    
+    try:
+        outFile = open(sys.argv[2], 'w')
+    except:
+        print usage
+        print 'Error opening output CSV file'
+        sys.exit(1)
+    
+    try:
+        numLines = int(sys.argv[3])
+    except:
+        print usage
+        print 'Number of lines must be an integer'
+        sys.exit(1)
+    
+    
+    for i in range(0, numLines):
+        outFile.write(inFile.readline().decode('utf-8', errors='ignore'))
+    
+    inFile.close()
+    outFile.close()
+    
+    print
+    print 'Done!'
+    print
+    
+if __name__ == '__main__':
+    main()
