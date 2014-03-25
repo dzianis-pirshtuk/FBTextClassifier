4cb6e0c84f3cadd6b6d6b17bd50e91817306ff1a
Noah E. Crocker <necrocke@umich.edu>
2014-03-25 13:27:00 -0400

    tokenize.py now commits to DB, uses server-side cursor, and does bulk updates

--- tokenize.py
+++ tokenize.py
@@ -1,34 +1,49 @@
+#!/usr/bin/python
 import psycopg2
 import happyfuntokenizing
+from collections import Counter
 
 conn = psycopg2.connect(database="MyPersonality", user="postgres",password="qwerty", host="localhost")
+# Set encoding to target DB encoding
+conn.set_client_encoding('WIN1252')
 
-status_cur = conn.cursor()
+# Make a server-side cursor (should fetch 2k at a time)
+status_cur = conn.cursor(name='status_cur')
 tf_cur = conn.cursor()
 
 tf_cur.execute("DROP TABLE IF EXISTS user_tf")
 tf_cur.execute("create table user_tf (userid char(32), term varchar, termcnt integer, primary key(userid, term));")
 
+status_cur.itersize = 2000
 status_cur.execute("SELECT * FROM user_status")
 
-status_update = ()
+status_update = status_cur.fetchone()
 
 tokenizer = happyfuntokenizing.Tokenizer(preserve_case = False)
 
+
 while status_update != None:
 
+	print "status: " + str(status_cur.rownumber) + "/" + str(status_cur.rowcount) + " " + str(status_update)
+	
+	status_term_count = Counter()
+
+	terms = tokenizer.tokenize(status_update[2])	
+
+	for token in terms:
+		status_term_count[token] += 1
+
+	for token, cnt in status_term_count.items():
+
+		update_cmd = "UPDATE user_tf SET termcnt=termcnt+%s WHERE userid=%s AND term=%s;"
+
+		insert_cmd = "INSERT INTO user_tf (userid, term, termcnt) SELECT %s, %s, %s WHERE NOT EXISTS (SELECT 1 FROM user_tf WHERE userid=%s AND term=%s);"
+
+       		print token
+		tf_cur.execute(update_cmd, (cnt, status_update[0], token))
+		tf_cur.execute(insert_cmd, (status_update[0], token, cnt, status_update[0], token))
+	
 	status_update = status_cur.fetchone()
 
-	terms = tokenizer.tokenize(status_update[2])
-
-	print "status"
-
-	for term in terms:
-
-		update_cmd = "UPDATE user_tf SET termcnt=termcnt+1 WHERE userid='" + status_update[0] + "' AND term='" + term + "';"
-
-		insert_cmd = "INSERT INTO user_tf (userid, term, termcnt) SELECT '" + status_update[0] + "', '" + term + "', 5 WHERE NOT EXISTS (SELECT 1 FROM user_tf WHERE userid='" + status_update[0] + "' AND term='" + term + "');"
-
-       
-		tf_cur.execute(update_cmd)
-		tf_cur.execute(insert_cmd)
+#commit changes back to DB for all status updates and begin new XACT
+conn.commit()
