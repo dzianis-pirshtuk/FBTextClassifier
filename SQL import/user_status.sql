e4de1f579b3152a101a01b345cf9e14c5fb9905f
Noah E. Crocker <necrocke@umich.edu>
2014-03-27 12:30:13 -0400

    Updated user_status import script for fast userid queries (runs in 30 minutes)

--- SQL import/user_status.sql
+++ SQL import/user_status.sql
@@ -1,13 +1,28 @@
 DROP TABLE IF EXISTS user_status;
 
-commit;
+COMMIT;
 
-CREATE TABLE user_status (
-	userid char(32) NOT NULL,
-	postdate varchar NOT NULL,
-	posttext text
+CREATE TABLE user_status 
+(
+  userid     CHAR(32) NOT NULL,
+  postdate   VARCHAR NOT NULL,
+  posttext   text
 );
 
-COPY user_status FROM '/tmp/user_status.csv' DELIMITER ',' CSV ENCODING 'WIN1252' ESCAPE '/' HEADER;
+COPY user_status
+FROM '/tmp/user_status.csv' DELIMITER ',' CSV ENCODING 'WIN1252' ESCAPE '/' HEADER;
+COMMIT;
 
-commit;
+CREATE INDEX useridindx ON user_status USING btree (userid);
+
+CLUSTER useridindx ON user_status;
+
+COMMIT;
+
+SET AUTOCOMMIT TRUE;
+
+VACUUM;
+
+ANALYZE;
+
+SET AUTOCOMMIT FALSE;
