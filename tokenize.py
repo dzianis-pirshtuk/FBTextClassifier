1ac6ff5cd95c72fcaee94b5de2483ab3a55ae544
Noah E. Crocker <necrocke@umich.edu>
2014-03-27 14:42:46 -0400

    Changed tokenize to use new import script (we should do a test to see which is faster. Almost positive it's this new one.)

--- tokenize.py
+++ tokenize.py
@@ -3,47 +3,43 @@ import psycopg2
 import happyfuntokenizing
 from collections import Counter
 
+#Setup global objects
 conn = psycopg2.connect(database="MyPersonality", user="postgres",password="qwerty", host="localhost")
-# Set encoding to target DB encoding
-#conn.set_client_encoding('WIN1252')
+tokenizer = happyfuntokenizing.Tokenizer(preserve_case = False)
+insert_cmd = "INSERT INTO user_tf (userid, term, termcnt) VALUES(%s, %s, %s)"
+select_cmd = "SELECT * FROM user_status WHERE userid=%s"
 
-# Make a server-side cursor (should fetch 2k at a time)
-status_cur = conn.cursor(name='status_cur')
+# user status selector curr
+status_cur = conn.cursor()
+
+# Destroy-Create target table
 tf_cur = conn.cursor()
-
 tf_cur.execute("DROP TABLE IF EXISTS user_tf")
 tf_cur.execute("create table user_tf (userid char(32), term varchar, termcnt integer, primary key(userid, term));")
 
-status_cur.itersize = 2000
-status_cur.execute("SELECT * FROM user_status")
+# Make a server-side cursor to get list of userids (should fetch 2k at a time)
+userid_cur = conn.cursor(name='userid_cur')
 
-status_update = status_cur.fetchone()
+userid_cur.itersize = 2000
+userid_cur.execute("SELECT DISTINCT userid FROM user_status")
+userid = userid_cur.fetchone()[0]
 
-tokenizer = happyfuntokenizing.Tokenizer(preserve_case = False)
-
-
-while status_update != None:
-
-	print "status: " + str(status_cur.rownumber) + "/" + str(status_cur.rowcount) + " " + str(status_update)
-	
+# Get all of the status updates for each userid in a loop
+while userid != None:
+	print "userid: " + userid
 	status_term_count = Counter()
-
-	terms = tokenizer.tokenize(status_update[2])	
-
-	for token in terms:
-		status_term_count[token] += 1
-
-	for token, cnt in status_term_count.items():
-
-		update_cmd = "UPDATE user_tf SET termcnt=termcnt+%s WHERE userid=%s AND term=%s;"
-
-		insert_cmd = "INSERT INTO user_tf (userid, term, termcnt) SELECT %s, %s, %s WHERE NOT EXISTS (SELECT 1 FROM user_tf WHERE userid=%s AND term=%s);"
-
-       		print token
-		tf_cur.execute(update_cmd, (cnt, status_update[0], token))
-		tf_cur.execute(insert_cmd, (status_update[0], token, cnt, status_update[0], token))
-	
+	status_cur.execute(select_cmd, (userid, ))
 	status_update = status_cur.fetchone()
+	# For each status update, add to TF counter
+	while status_update != None:
+		terms = tokenizer.tokenize(status_update[2])
+		for token in terms:
+			status_term_count[token] += 1
+	       		#print token
+		status_update = status_cur.fetchone()
+	for token, cnt in status_term_count.items():
+		tf_cur.execute(insert_cmd, (userid, token, cnt))
+	userid = userid_cur.fetchone()[0]
 
-#commit changes back to DB for all status updates and begin new XACT
+#commit changes back to DB
 conn.commit()
