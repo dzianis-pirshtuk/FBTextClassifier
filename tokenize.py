2c6f6df6a67c8f11f1fa28afe348f4a3aa948330
Noah E. Crocker <necrocke@umich.edu>
2014-03-31 00:28:47 -0400

    Updated tokenizer, runs in 4 hours

--- tokenize.py
+++ tokenize.py
@@ -7,39 +7,43 @@ from collections import Counter
 conn = psycopg2.connect(database="MyPersonality", user="postgres",password="qwerty", host="localhost")
 tokenizer = happyfuntokenizing.Tokenizer(preserve_case = False)
 insert_cmd = "INSERT INTO user_tf (userid, term, termcnt) VALUES(%s, %s, %s)"
-select_cmd = "SELECT * FROM user_status WHERE userid=%s"
 
 # user status selector curr
-status_cur = conn.cursor()
+status_cur = conn.cursor("status_cur")
+status_cur.execute("SELECT * FROM user_status ORDER BY userid;")
 
 # Destroy-Create target table
 tf_cur = conn.cursor()
 tf_cur.execute("DROP TABLE IF EXISTS user_tf")
 tf_cur.execute("create table user_tf (userid char(32), term varchar, termcnt integer, primary key(userid, term));")
 
-# Make a server-side cursor to get list of userids (should fetch 2k at a time)
-userid_cur = conn.cursor(name='userid_cur')
-
-userid_cur.itersize = 2000
-userid_cur.execute("SELECT DISTINCT userid FROM user_status")
-userid = userid_cur.fetchone()[0]
-
 # Get all of the status updates for each userid in a loop
-while userid != None:
-	print "userid: " + userid
-	status_term_count = Counter()
-	status_cur.execute(select_cmd, (userid, ))
-	status_update = status_cur.fetchone()
+status = status_cur.fetchone()
+userid = status[0]
+status_term_count = Counter()
+	
+		
+
+print "userid: " + userid
+while status != None:
 	# For each status update, add to TF counter
-	while status_update != None:
-		terms = tokenizer.tokenize(status_update[2])
-		for token in terms:
-			status_term_count[token] += 1
-	       		#print token
-		status_update = status_cur.fetchone()
-	for token, cnt in status_term_count.items():
-		tf_cur.execute(insert_cmd, (userid, token, cnt))
-	userid = userid_cur.fetchone()[0]
+	terms = tokenizer.tokenize(status[2])
+	for token in terms:
+		status_term_count[token] += 1
+		#print token
+	status = status_cur.fetchone()
+	if status == None or userid != status[0]:
+		tup = []
+		for token, cnt in status_term_count.items():
+			if token != None:
+				tup.append((userid, token, cnt))
+		if(len(tup) > 0):
+			args_str = ",".join(cur.mogrify("(%s, %s, %s)", x) for x in tup)
+			tf_cur.execute("INSERT INTO user_tf (userid, term, termcnt) VALUES " + args_str)
+		status_term_count = Counter()
+		if status != None:		
+			userid = status[0]
+			print "userid: " + userid
 
 #commit changes back to DB
 conn.commit()
